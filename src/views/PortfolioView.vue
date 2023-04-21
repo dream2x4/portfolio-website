99a4c0b8480f377dc9c69666f0be0d85d6e3e25b
dream2x4 <andersenthanakit@gmail.com>
2023-04-21 17:07:30 +0200

    replaced content

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,54 +1,228 @@
 <template>
-    <div>
-        Portfolio
+<div class="nav-sub" id="top"></div>
+  <div class="portfolio-title-container">
+    <div class="portfolio-title">MY PORTFOLIO</div>
+    <div class="filter-box d-flex justify-content-start">
+        <button type="button" class="filter-btn UX" @click="$event => changeCategory('UX')" value="UX">UX</button>
+        <button type="button" class="filter-btn Video" @click="$event => changeCategory('Video')" value="Video">Video</button>
+        <button type="button" class="filter-btn Branding" @click="$event => changeCategory('Branding')" value="Branding">Branding</button>
+        <button type="button" class="filter-btn Web" @click="$event => changeCategory('Web')" value="Web">Web</button>
+        <button type="button" class="filter-btn All" @click="$event => changeCategory('All')">All</button>
+      </div>
+  </div>
+  <div class="portfolio-container d-flex justify-content-center">
+    <div class="portfolio">
         <!-- {{ state }} -->
         <div class="portfolio-item-container">
         <div v-for="item in state " :key="item" class="portfolio-item">
-            <p class="p-category" :class="item.category">{{ item.category }}</p>
-            <RouterLink :to ="{name: 'portfoliodetails', params:{id: item.id}}" >
-                <h2>{{ item.title }}</h2>
+            <RouterLink :to ="{name: 'portfoliodetails', params:{id: item.id}}" class="portfolio-box">
+              <img :src="item.image" alt="" class="portfolio-img">
+              <p class="p-category" :class="item.category">{{ item.category }}</p>
+              <h2>{{ item.title }}</h2>
             </RouterLink>
-            <p>{{ item.description }}</p>
-            <a href="https://www.google.com">LinkedIn</a>
-            <a :href="item.link"></a>
-            <img :src="item.image" alt="">
         </div>
         </div>
     </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import portfoliodb from '../modules/portfoliodb'
 
-const { state } = portfoliodb()
+const isActive = ref(true)
+
+let { state } = portfoliodb()
+const originalState = state.value;
+
+var selector
+
+  function changeCategory(category) {
+    if(category != "All"){
+      selector = category
+      state.value = originalState.filter(filterFunc)
+    } else{
+      console.log("ye")
+      state.value = originalState
+    }
+  }
+
+  var filterFunc = (item) => {
+    var category = item.category;
+
+    if(category === selector) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+
 
 </script>
 
 <style scoped>
+
+.portfolio-title-container {
+  padding-top: 6rem;
+  background-color: var(--vt-c-grey);
+}
+
+.portfolio-container {
+  margin: 0 4rem;
+}
+
+.portfolio {
+  padding: 4rem 0 6rem 0;
+  width: 100%;
+}
+
 .portfolio-item-container {
     display: flex;
+    position: relative;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 100%;
+}
+
+.portfolio-box {
+  background-color: #0e0e0e;
+}
+
+.filter-box {
+  flex-wrap: wrap;
+}
+
+.filter-btn {
+  border: none var(--vt-c-white-soft);
+  background-color: transparent;
+  font-family: 'GlacialIndifference-Bold';
+  color: var(--vt-c-white-soft);
+  font-size: 1rem;
+  display: flex;
+  justify-content: flex-start;
+  text-transform: uppercase;
+  margin-top: 2rem;
+}
+
+.filter-box :first-child {
+  margin-left: 4rem;
+}
+
+.filter-btn:hover {
+  color: var(--vt-c-purple);
+  border-color: var(--vt-c-purple);
+  background-color: var(--vt-c-white-soft);
 }
 
 .portfolio-item {
-    margin: 12px;
-    padding: 12px;
-    border:2px solid #ededed;
+    padding: 0;
     display: flex;
     flex-direction: column;
+    transition: 0.1s;
+    width: 20%;
+    padding: 6px;
+}
+
+.portfolio-item:hover {
+  scale: 1.05;
+}
+
+h2 {
+  font-size: 2rem;
+  padding: 0 0.4rem 0.8rem 0.4rem;
+  margin: 0;
+  font-family: 'GlacialIndifference-Bold';
 }
 
 .p-category {
     font-weight: 700;
     text-transform: uppercase;
+    margin: 0.4rem 0.4rem 0 0.4rem;
+} 
+
+.portfolio-img{
+  width: 100%;
+  height: auto;
 }
 
-.Web {
-    color: #e17821;
-    border-left: 1px solid #e17821;
+.All {
+  color: 	#DC143C;
+  border-left: 1px solid 	#DC143C;
+}
+
+.UX {
+  color: #0096FF;
+  border-left: 1px solid #0096ff;
 }
 
 .Video {
     color: #3fc914;
     border-left: 1px solid #3fc914;
+}
+
+.Branding {
+  color: #fced23;
+  border-left: 1px solid #fced23;
+}
+
+.Web {
+    color: #FFAC1C;
+    border-left: 1px solid #FFAC1C;
+}
+
+@media (hover: hover) {
+  a:hover {
+    color: var(--vt-c-white-soft);
+  }
+}
+
+@media (max-width: 992px){
+  .portfolio {
+    padding: 2rem 0 6rem 0;
+  }
+
+  .portfolio-item {
+    width: 50%;
+    padding: 12px;
+  }
+}
+
+@media (max-width: 576px){
+
+.portfolio-container {
+  margin-left: 2rem;
+}
+
+h2 {
+  font-size: 1.33rem;
+  padding-bottom: 0.53rem;
+}
+
+.filter-btn {
+  font-size: 0.8rem;
+}
+
+.filter-btn:first-child {
+  margin-left: 2rem;
+}
+
+.filter-box {
+  justify-content: center;
+}
+
+.p-category {
+  font-size: 10.67px;
+}
+
+  .portfolio-title-container{
+    padding-top: 4rem;
+  }
+
+
+  .portfolio-title{
+    font-size: 1.5rem;
+    margin-left: 2rem;
+  }
 }
 </style>
